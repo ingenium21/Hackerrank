68b7ac34742a156628f3cf61fa8ca896f5509968
Renato Regalado <renato.regalado@gmail.com>
2019-08-06 14:18:54 -0500

    added new files for linked lists

---
+++ Python3/Algorithms/DataStructures/LinkedList/insertNodeAtSpecificPosition.py
@@ -0,0 +1,70 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+class SinglyLinkedListNode:
+    def __init__(self, node_data):
+        self.data = node_data
+        self.next = None
+
+class SinglyLinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def insert_node(self, node_data):
+        node = SinglyLinkedListNode(node_data)
+
+        if not self.head:
+            self.head = node
+        else:
+            self.tail.next = node
+
+
+        self.tail = node
+
+def print_singly_linked_list(node, sep, fptr):
+    while node:
+        fptr.write(str(node.data))
+
+        node = node.next
+
+        if node:
+            fptr.write(sep)
+
+# Complete the insertNodeAtPosition function below.
+def insertNodeAtPosition(head, data, position):
+    n = head #set variable n to equal head
+    for _ in range(position - 1):
+        n = n.next #loop thru llist to get to your desired position
+    n_next = n.next #new var to equal the next item
+    n.next = SinglyLinkedListNode(data) #now set n.next to equal desired number
+    n.next.next = n_next #and set the number after that to equal n_next
+    return head
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    llist_count = int(input())
+
+    llist = SinglyLinkedList()
+
+    for _ in range(llist_count):
+        llist_item = int(input())
+        llist.insert_node(llist_item)
+
+    data = int(input())
+
+    position = int(input())
+
+    llist_head = insertNodeAtPosition(llist.head, data, position)
+
+    print_singly_linked_list(llist_head, ' ', fptr)
+    fptr.write('\n')
+
+    fptr.close()
