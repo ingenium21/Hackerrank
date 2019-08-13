12f1e13d556ef90bb21ae908129054d6ef11df9b
Renato Regalado <renato.regalado@gmail.com>
2019-08-13 14:36:42 -0500

    Create treePreorderTraversal.py

---
+++ Python3/DataStructures/Trees/treePreorderTraversal.py
@@ -0,0 +1,68 @@
+class Node:
+    def __init__(self, info): 
+        self.info = info  
+        self.left = None  
+        self.right = None 
+        self.level = None 
+
+    def __str__(self):
+        return str(self.info) 
+
+class BinarySearchTree:
+    def __init__(self): 
+        self.root = None
+
+    def create(self, val):  
+        if self.root == None:
+            self.root = Node(val)
+        else:
+            current = self.root
+         
+            while True:
+                if val < current.info:
+                    if current.left:
+                        current = current.left
+                    else:
+                        current.left = Node(val)
+                        break
+                elif val > current.info:
+                    if current.right:
+                        current = current.right
+                    else:
+                        current.right = Node(val)
+                        break
+                else:
+                    break
+
+"""
+Node is defined as
+self.left (the left child of the node)
+self.right (the right child of the node)
+self.info (the value of the node)
+"""
+def preOrder(root):
+    #preorder traversal:
+    # 1. Check if the current node is empty
+    # 2. Display the data part of the root (or current node)
+    # 3. traverse the left subtree by recursively calling the preOrder function
+    # 4. traverse the right subtree by recursively calling the preOrder function
+    # Root -> Left -> Right
+    #Write your code here
+    if root:
+        print(root.info, end=" ")
+        if root.left:
+            preOrder(root.left)
+        if root.right:
+            preOrder(root.right)
+
+
+
+tree = BinarySearchTree()
+t = int(input())
+
+arr = list(map(int, input().split()))
+
+for i in range(t):
+    tree.create(arr[i])
+
+preOrder(tree.root)
