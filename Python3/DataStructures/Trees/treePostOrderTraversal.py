0bd8d496959ced3fbb205c0e55b1fc89eaf73f51
Renato Regalado <renato.regalado@gmail.com>
2019-08-15 11:41:41 -0500

    postOrder traversal and InOrder Traversal files added

---
+++ Python3/DataStructures/Trees/treePostOrderTraversal.py
@@ -0,0 +1,64 @@
+class Node:
+    def __init__(self, info): 
+        self.info = info  
+        self.left = None  
+        self.right = None 
+        self.level = None 
+
+    def __str__(self):
+        return str(self.info) 
+
+class BinarySearchTree:
+    def __init__(self): 
+        self.root = None
+
+    def create(self, val):  
+        if self.root == None:
+            self.root = Node(val)
+        else:
+            current = self.root
+         
+            while True:
+                if val < current.info:
+                    if current.left:
+                        current = current.left
+                    else:
+                        current.left = Node(val)
+                        break
+                elif val > current.info:
+                    if current.right:
+                        current = current.right
+                    else:
+                        current.right = Node(val)
+                        break
+                else:
+                    break
+
+"""
+Node is defined as
+self.left (the left child of the node)
+self.right (the right child of the node)
+self.info (the value of the node)
+"""
+# PostOrder Traversal:
+# 1. Check if the current node is empty
+# 2. recursively run down the left side first
+# 3. when it reaches the end of the left side, it will go down the right
+# 4. print that value
+# 5. left->right->root
+def postOrder(root):
+    if root:
+        postOrder(root.left)
+        postOrder(root.right)
+        print(root.info, end=" ")
+    
+
+tree = BinarySearchTree()
+t = int(input())
+
+arr = list(map(int, input().split()))
+
+for i in range(t):
+    tree.create(arr[i])
+
+postOrder(tree.root)
