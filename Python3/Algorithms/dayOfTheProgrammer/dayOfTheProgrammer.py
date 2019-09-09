f1eea273ba1000e999d314d42ca15b01252e1263
Renato Regalado <renato.regalado@gmail.com>
2019-09-09 14:42:29 -0500

    added comments

--- Python3/Algorithms/dayOfTheProgrammer/dayOfTheProgrammer.py
+++ Python3/Algorithms/dayOfTheProgrammer/dayOfTheProgrammer.py
@@ -6,7 +6,11 @@ import random
 import re
 import sys
 
-# Complete the dayOfProgrammer function below.
+#Using f strings since its python3
+#this was deceptively simple, but basically the question sort of gave you the answer
+#if you're in a leap year it almost always lands on the 12th, if not, it lands on the 13th
+#so rather than do any weird calculations trying to figure out what day it was, i just told it o return one of those days
+#exception being 1918, when the transition happened, they skipped 14 days so it has to return 26th of september.
 def dayOfProgrammer(year):
     if year >= 1919:
         if year % 400 == 0 or (year % 4 == 0 and year % 100 != 0):
